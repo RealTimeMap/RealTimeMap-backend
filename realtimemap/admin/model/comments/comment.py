2c1d096ae4ab6807edab25ac33ef389a296f0353
WinerTy <nikitokmay@gmail.com>
2025-12-03 02:28:20 +0500

    Fix: Admin form for Comment model

--- realtimemap/admin/model/comments/comment.py
+++ realtimemap/admin/model/comments/comment.py
@@ -1,6 +1,10 @@
+from typing import Dict, Any
+
 from sqlalchemy import Select
 from sqlalchemy.orm import joinedload
 from starlette.requests import Request
+from starlette_admin import TextAreaField, HasOne
+from starlette_admin.exceptions import FormValidationError
 
 from admin.model.base import BaseModelAdmin
 from modules import Comment
@@ -9,15 +13,29 @@ from modules import Comment
 class AdminComment(BaseModelAdmin):
     fields = [
         Comment.id,
-        Comment.mark,
-        Comment.content,
-        Comment.parent,
-        Comment.owner,
+        HasOne("mark", identity="mark", required=True, label="Mark"),
+        HasOne("owner", identity="user", required=True, label="Owner"),
+        TextAreaField("content", required=True),
+        HasOne("parent", identity="comment", label="Parent comment", required=False),
         Comment.stats,
     ]
 
     exclude_fields_from_create = [Comment.stats]
     exclude_fields_from_edit = [Comment.stats]
+
+    async def validate(self, request: Request, data: Dict[str, Any]) -> None:
+        errors: Dict[str, str] = dict()
+
+        if data["parent"]:
+            parent_comment: "Comment" = data["parent"]
+
+            if parent_comment.parent_id:
+                errors["parent"] = "Nesting limit exceeded. Cannot reply to this reply."
+
+        if len(errors) > 0:
+            raise FormValidationError(errors)
+
+        return await super().validate(request, data)
 
     def get_list_query(self, request: Request) -> Select:
         stmt = super().get_list_query(request)
