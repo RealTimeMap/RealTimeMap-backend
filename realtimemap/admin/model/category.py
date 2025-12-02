f1a366c3180d14e0b48cab381582a1ac5330af5f
WinerTy <nikitokmay@gmail.com>
2025-12-03 03:04:14 +0500

    Upd: Color validation

--- realtimemap/admin/model/category.py
+++ realtimemap/admin/model/category.py
@@ -1,5 +1,11 @@
+from typing import Dict, Any
+
+from pydantic_core import PydanticCustomError
+from pydantic_extra_types.color import Color
+from starlette.requests import Request
 from starlette_admin import ColorField
 from starlette_admin.contrib.sqla import ModelView
+from starlette_admin.exceptions import FormValidationError
 
 from modules import Category
 
@@ -13,3 +19,18 @@ class AdminCategory(ModelView):
         Category.icon,
         Category.is_active,
     ]
+
+    async def validate(self, request: Request, data: Dict[str, Any]) -> None:
+        errors: Dict[str, str] = dict()
+
+        if data.get("color", None):
+            color: str = data.get("color")
+            try:
+                Color(color)
+            except PydanticCustomError:
+                errors["color"] = "Invalid color"
+
+        if len(errors) > 0:
+            raise FormValidationError(errors)
+
+        return await super().validate(request, data)
